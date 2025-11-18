1c54b79dfb82dac45e859063071f4618416a18eb
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-11-19 11:36:36 +1300

    feat: configure routes and remove placeholder `app.html`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'chat',
+    loadComponent: () =>
+      import('./pages/chat/chat').then((com) => com.Chat),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then((com) => com.Login),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/login/login').then((com) => com.Login),
+  },
+];
